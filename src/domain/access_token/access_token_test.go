6d3bd6a16109498dfd910f8a507315dc1cd279e8
Danjel Hysenaj <danjel.hysenaj@nl.abnamro.com>
2024-03-05 12:58:56 +0100

    added access token handler test cases

--- src/domain/access_token/access_token_test.go
+++ src/domain/access_token/access_token_test.go
@@ -1,7 +1,15 @@
 package access_token
 
-import "testing"
+import (
+	"testing"
+	"time"
+)
 
+func TestAccessTokenConstants(t *testing.T) {
+	if expirationTime != 24 {
+		t.Error("expiration time should be 24 hours")
+	}
+}
 func TestGetNewAccessToken(t *testing.T) {
 	at := GetNewAccessToken()
 	if at.IsExpired() {
@@ -12,5 +20,16 @@ func TestGetNewAccessToken(t *testing.T) {
 	}
 	if at.UserId != 0 {
 		t.Error("new access token should not have an associated user id")
+	}
+}
+
+func TestAccessToken_IsExpired(t *testing.T) {
+	at := AccessToken{}
+	if !at.IsExpired() {
+		t.Error("empty access token should be expired by default")
+	}
+	at.Expires = time.Now().UTC().Add(3 * time.Hour).Unix()
+	if at.IsExpired() {
+		t.Error("access token expiring three hours from now should not be expired")
 	}
 }
