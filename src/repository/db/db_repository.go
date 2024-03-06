887e519f90246bcc3a972fe35560fcacea1594e1
Danjel Hysenaj <danjel.hysenaj@nl.abnamro.com>
2024-03-06 15:58:14 +0100

    added handling error getting session for cassandra

--- src/repository/db/db_repository.go
+++ src/repository/db/db_repository.go
@@ -1,6 +1,7 @@
 package db
 
 import (
+	"github.com/danjelhysenaj-dev/bookstore_auth-api/src/clients/cassandra"
 	"github.com/danjelhysenaj-dev/bookstore_auth-api/src/domain/access_token"
 	"github.com/danjelhysenaj-dev/bookstore_auth-api/src/utils/errors"
 )
@@ -16,5 +17,10 @@ type dbRepository struct {
 }
 
 func (r *dbRepository) GetById(id string) (*access_token.AccessToken, *errors.RestErr) {
+	//TODO: Implement get access token from CassandraDB.
+	_, err := cassandra.GetSession()
+	if err != nil {
+		panic(err)
+	}
 	return nil, errors.NewInternalServerError("database connection not implemented yet")
 }
