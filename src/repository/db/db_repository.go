cb7e66b9f280ec6477881f6c532048c8d34cffbb
Danjel Hysenaj <danjel.hysenaj@nl.abnamro.com>
2024-03-07 12:01:53 +0100

    fixed all issues connection to cassandra

--- src/repository/db/db_repository.go
+++ src/repository/db/db_repository.go
@@ -6,6 +6,10 @@ import (
 	"github.com/danjelhysenaj-dev/bookstore_auth-api/src/utils/errors"
 )
 
+const (
+	queryGetAccessToken = "SELECT access_token, user_id, client_id, expires FROM access_tokens WHERE access_token=?;"
+)
+
 func NewRepository() DbRepository {
 	return &dbRepository{}
 }
@@ -17,10 +21,14 @@ type dbRepository struct {
 }
 
 func (r *dbRepository) GetById(id string) (*access_token.AccessToken, *errors.RestErr) {
-	//TODO: Implement get access token from CassandraDB.
-	_, err := cassandra.GetSession()
-	if err != nil {
-		panic(err)
+	var result access_token.AccessToken
+	if err := cassandra.GetSession().Query(queryGetAccessToken, id).Scan(
+		&result.AccessToken,
+		&result.UserId,
+		&result.ClientId,
+		&result.Expires,
+	); err != nil {
+		return nil, errors.NewInternalServerError(err.Error())
 	}
-	return nil, errors.NewInternalServerError("database connection not implemented yet")
+	return &result, nil
 }
